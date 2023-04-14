f398dd0387fd90ea5d9291ce483418831d2c8fcd
Sergii Lovygin <sergey.lovygin@yahoo.com>
2023-04-15 01:29:23 +0300

    add core for settings support

---
+++ telegram_explorer/__init__.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from json import dump
+from json import load
+from pathlib import Path
+from typing import Any
+from typing import Self
+
+
+def settings_directory() -> Path:
+    """Return root settings directory path."""
+    return Path.home() / ".telegram-explorer"
+
+
+def settings_file() -> Path:
+    """Return settings file path."""
+    return settings_directory() / "settings.json"
+
+
+@dataclass(slots=True, kw_only=True)
+class Settings:
+    api_id: int
+    api_hash: str
+    path: Path = settings_file()
+
+    def __post_init__(self) -> None:
+        if not self._validate_api():
+            raise ValueError("Settings initialized with wrong values")
+
+    @classmethod
+    def load(cls, path: Path) -> Self:
+        if not path.is_file():
+            raise ValueError(f"path ({path}) is not a file.")
+
+        with open(path, encoding="utf-8") as file:
+            raw_settings: dict[str, Any] = load(file)
+
+        return cls(path=path, api_id=int(raw_settings.get("api_id", 0)), api_hash=raw_settings.get("api_hash", ""))
+
+    def save(self) -> None:
+        path = self.path if not self.path.exists() else self.path.with_suffix(".new")
+
+        data = dict(api_id=self.api_id, api_hash=self.api_hash)
+        with open(path, "w", encoding="utf-8") as file:
+            dump(data, file, indent=4)
+
+        if path is not self.path:
+            path.replace(self.path)
+
+    def _validate_api(self) -> bool:
+        return bool(self.api_id) and bool(self.api_hash)
--- telegram_explorer/telegram_history.py
+++ telegram_explorer/telegram_history.py
@@ -7,22 +7,34 @@ from click import DateTime
 from click import option
 from pyrogram import Client
 
+from telegram_explorer import Settings
+from telegram_explorer import settings_directory
+from telegram_explorer import settings_file
+
+
+def get_settings(api: tuple[int, str], path: Path) -> Settings:
+    if len(api) == 2:
+        return Settings(api_id=api[0], api_hash=api[1])
+
+    return Settings.load(path)
+
 
 def get_session_dir() -> Path:
-    root = Path.home() / ".telegram-explorer"
+    """Return the settings root directory, but ensure it exists."""
+    root = settings_directory()
     root.mkdir(parents=True, exist_ok=True)
     return root
 
 
 @command()
-@argument("api-id", type=int)
-@argument("api-hash", type=str)
+@argument("api", type=tuple[int, str], default=tuple())
 @option("--chat", type=str, help="Chat name or id")
 @option("-s", "--start", type=DateTime(formats=["%d.%m.%Y"]), default="", help="Start date")
 @option("-e", "--end", type=DateTime(formats=["%d.%m.%Y"]), default="", help="End date")
-def cli(api_id: int, api_hash: str, chat: str, start: datetime, end: datetime) -> None:
+def cli(api: tuple[int, str], chat: str, start: datetime, end: datetime) -> None:
+    config = get_settings(api, settings_file())
     session_dir = get_session_dir()
-    client = Client("telegram-history", api_id, api_hash, workdir=str(session_dir), hide_password=True)
+    client = Client("telegram-history", config.api_id, config.api_hash, workdir=str(session_dir), hide_password=True)
     with client:
         grabber = client.get_chat_history(chat_id=chat, limit=0, offset_date=end)
         history = [msg for msg in grabber if msg.date.timestamp() >= start.timestamp()]
