aba99fbfe2016c6c88ecc60e70059b42666e44db
Sergii Lovygin <sergey.lovygin@yahoo.com>
2023-04-24 01:57:35 +0300

    add the implementation of the login form business logic

--- telegram_explorer/gui/core/window.py
+++ telegram_explorer/gui/core/window.py
@@ -1,7 +1,38 @@
 """Helpful set of window-related operations."""
+from tkinter import GROOVE
 from tkinter import Misc
 from tkinter import Tk
 from tkinter import Toplevel
+from tkinter.ttk import Frame
+from tkinter.ttk import Label
+from tkinter.ttk import Progressbar
+
+
+def show_wait_window(parent: Toplevel, message: str, interval: int = 5) -> Toplevel:
+    parent.attributes("-disabled", True)
+
+    waiter = Toplevel(parent)
+    waiter.resizable(False, False)
+    waiter.transient(parent)
+
+    def close() -> None:
+        parent.attributes("-disabled", False)
+        waiter.destroy()
+
+    waiter.protocol("WM_DELETE_WINDOW", close)
+
+    frame = Frame(waiter, relief=GROOVE, borderwidth=5)
+    Label(frame, text=message).grid(row=0, column=0, sticky="ewns", padx=1, pady=1)
+
+    progress = Progressbar(frame, orient="horizontal", mode="indeterminate")
+    progress.grid(row=1, column=0, sticky="ewns", padx=1, pady=1)
+    frame.pack()
+
+    centralize_window(waiter, parent)
+    waiter.overrideredirect(True)
+
+    progress.start(interval)
+    return waiter
 
 
 def centralize_window(window: Tk | Toplevel, parent: Tk | Toplevel | Misc | None = None) -> None:
--- telegram_explorer/gui/forms/login_form.py
+++ telegram_explorer/gui/forms/login_form.py
@@ -5,6 +5,9 @@ It can be used in the standalone form to log in because it depends on the `teleg
 session data in the file.
 """
 from asyncio import AbstractEventLoop
+from enum import auto
+from enum import IntEnum
+from tkinter import messagebox
 from tkinter import StringVar
 from tkinter import Tk
 from tkinter import Toplevel
@@ -12,9 +15,15 @@ from tkinter.ttk import Button
 from tkinter.ttk import Entry
 from tkinter.ttk import Label
 
+from pyrogram import Client
+from pyrogram.errors import BadRequest
+from pyrogram.errors import SessionPasswordNeeded
+from pyrogram.types import User
+
 from telegram_explorer import Settings
 from telegram_explorer.gui.core.modal import FormResult
 from telegram_explorer.gui.core.modal import ModalForm
+from telegram_explorer.gui.core.window import show_wait_window
 
 
 class PhoneCodeForm(ModalForm):
@@ -26,18 +35,26 @@ class PhoneCodeForm(ModalForm):
 
         self._phone_code = StringVar(self, name="phone_code")
 
-        self._setup_layout()
+        self._create_controls()
 
     @property
-    def phone_code(self) -> int:
-        return int(self._phone_code.get())
+    def phone_code(self) -> str:
+        return self._phone_code.get()
 
-    def _setup_layout(self) -> None:
+    def _create_controls(self) -> None:
         Label(self, text="Code:").grid(row=0, column=0, sticky="ewns", padx=5, pady=5)
         Entry(self, textvariable=self._phone_code).grid(row=0, column=1, columnspan=2, sticky="ewns", padx=5, pady=5)
 
         Button(self, text="ok", command=self.ok).grid(row=1, column=0, columnspan=2, sticky="ewns", padx=5, pady=5)
         Button(self, text="cancel", command=self.cancel).grid(row=1, column=2, sticky="ewns", padx=5, pady=5)
+
+
+class LoginResult(IntEnum):
+    Success = auto()
+    PhoneNumberInvalid = auto()
+    CodeWrong = auto()
+    UserUnregistered = auto()
+    PasswordIncorrect = auto()
 
 
 class LoginForm(ModalForm):
@@ -61,22 +78,84 @@ class LoginForm(ModalForm):
 
         # Password
         Label(self, text="Password:").grid(row=1, column=0, sticky="ewns", padx=5, pady=5)
-        password = Entry(self, textvariable=self._password, show="*")
+        password = Entry(
+            self,
+            textvariable=self._password,
+            show="*",
+        )
         password.grid(row=1, column=1, columnspan=2, sticky="ewns", padx=5, pady=5)
 
         # Buttons
-        Button(self, text="Login", command=self._login).grid(row=2, column=1, sticky="ewns", padx=5, pady=5)
+        Button(self, text="Login", command=self._request_login).grid(row=2, column=1, sticky="ewns", padx=5, pady=5)
         Button(self, text="Cancel", command=self.cancel).grid(row=2, column=2, sticky="ewns", padx=5, pady=5)
 
-    def _login(self) -> None:
-        # TODO: implement login flow with Pyrogram.Client
-        confirm = PhoneCodeForm(self)
-        if confirm.show_modal() is FormResult.Cancel:
-            return self.cancel()
+    def _request_login(self) -> None:
+        if not self._phone_number.get():
+            messagebox.showerror(title="Error", message="Phone number is empty")
+            return
 
-        phone_number = self._phone_number.get()
-        if phone_number and phone_number != self._settings.phone_number:
-            self._settings.phone_number = phone_number
-            self._settings.save()
+        if not self._password.get():
+            messagebox.showerror(title="Error", message="Password is empty")
+            return
 
-        self.ok()
+        self._loop.create_task(self._login())
+
+    async def _login(self) -> None:
+        status = await self._do_login_process()
+        if status is LoginResult.Success:
+            phone_number = self._phone_number.get()
+            if phone_number != self._settings.phone_number:
+                self._settings.phone_number = phone_number
+                self._settings.save()
+            self.ok()
+
+        self._show_login_error(status)
+
+    async def _do_login_process(self) -> LoginResult:
+        """Adopted version of the code/logic extracted pyrogram.Client class."""
+        waiter = show_wait_window(self, message="Connecting to Telegram server...")
+
+        client = Client(
+            name="telegram-explorer",
+            api_id=self._settings.api_id,
+            api_hash=self._settings.api_hash,
+            phone_number=self._settings.phone_number,
+            workdir=str(self._settings.get_session_dir()),
+        )
+
+        try:
+            if await client.connect():
+                phone_number = self._phone_number.get()
+                try:
+                    sent_code = await client.send_code(phone_number)
+                    waiter.destroy()
+
+                    confirm = PhoneCodeForm(self)
+                    if confirm.show_modal() is FormResult.Cancel:
+                        return LoginResult.CodeWrong
+                except BadRequest:
+                    return LoginResult.PhoneNumberInvalid
+
+                try:
+                    signed_in = await client.sign_in(phone_number, sent_code.phone_code_hash, confirm.phone_code)
+                    return LoginResult.Success if isinstance(signed_in, User) else LoginResult.UserUnregistered
+                except BadRequest:
+                    return LoginResult.CodeWrong
+                except SessionPasswordNeeded:
+                    try:
+                        await client.check_password(self._password.get())
+                        return LoginResult.Success
+                    except BadRequest:
+                        return LoginResult.PasswordIncorrect
+        finally:
+            await client.disconnect()
+            waiter.destroy()
+
+        return LoginResult.Success
+
+    @staticmethod
+    def _show_login_error(error: LoginResult) -> None:
+        if error is LoginResult.Success:
+            return
+
+        messagebox.showerror("Login Error", str(error))
