26a3ebc2840f19546cbd2f0461aced8bbc02b486
Sergii Lovygin <sergey.lovygin@yahoo.com>
2023-04-23 22:39:09 +0300

    enhance implementations of the `ModalForm` concept
    - initial zero-geometry to allow "auto" resize for the subclasses
    - become modal on demand instead of since creation
    - do small improvements/cleanups

--- telegram_explorer/gui/core/modal.py
+++ telegram_explorer/gui/core/modal.py
@@ -2,6 +2,9 @@ from enum import auto
 from enum import IntEnum
 from tkinter import Tk
 from tkinter import Toplevel
+from typing import cast
+
+from telegram_explorer.gui.core.misc import centralize_window
 
 
 class FormResult(IntEnum):
@@ -10,18 +13,23 @@ class FormResult(IntEnum):
 
 
 class ModalForm(Toplevel):
-    def __init__(self, parent: Tk | Toplevel) -> None:
-        super().__init__(parent, bg=parent.cget("bg"))
+    def __init__(self, parent: Tk | Toplevel, title: str = "") -> None:
+        super().__init__(parent)
+        self.title(title if title else parent.title())
         self.resizable(False, False)
+        self.geometry("")
 
-        self._modal_result = FormResult.Ok
-
-        self.wait_visibility()
-        self.grab_set()
-        self.transient(parent)
+        self._modal_result = FormResult.Cancel
+        self.protocol("WM_DELETE_WINDOW", self.close)
 
     def show_modal(self) -> FormResult:
-        self._center()
+        self.wait_visibility()
+        self.grab_set()
+
+        if not isinstance(self.master, Tk):
+            self.transient(cast(Toplevel, self.master))
+
+        centralize_window(self, self.master)
         self.master.wait_window(self)
         return self._modal_result
 
@@ -36,18 +44,3 @@ class ModalForm(Toplevel):
     def close(self) -> None:
         self.grab_release()
         self.destroy()
-
-    def _center(self) -> None:
-        width = self.winfo_width()
-        height = self.winfo_height()
-
-        master_x = self.master.winfo_x()
-        master_y = self.master.winfo_y()
-        master_width = self.master.winfo_width()
-        master_height = self.master.winfo_height()
-
-        # Calculate Starting X and Y coordinates for Window
-        x = master_x + (master_width - width) // 2
-        y = master_y + (master_height - height) // 2
-
-        self.geometry(f"+{x}+{y}")
